0b8c6874a943182c73c166edfe5b736575b7b06a
Paul Emmett <pemmett2@gmail.com>
2015-01-24 22:33:11 -0600

    eGFR Shiny App
    This is a very simple shiny app which calculates Glomerular Filtration
    Rate (eGFR) as a function of serum creatinine for any given age which
    the user selects with the slider widget

---
+++ UI.R
@@ -0,0 +1,15 @@
+shinyUI(fluidPage(
+  headerPanel("GFR plot"
+              ),
+  sidebarPanel(
+    h4("Select the age using the slider, the application will graph Glomular Filtration Rate as a function of serum Creatinine"),
+    p("This is an estimated value for non African American males"),
+    p("This is only at estimate: it has a wide confidence interval and requires a trained nephrologist to interpret these numbers in context"),
+    p("It is not to be used as a diagnosis of kidney function"),
+    sliderInput('age', 'Select Age',value = 21, min = 21, max = 80, step = 0.05,)
+  ),
+  mainPanel(
+    plotOutput('gfrPlot')
+  )
+))
+# s
